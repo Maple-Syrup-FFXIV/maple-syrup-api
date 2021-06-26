bd0dad8e524b99a7f5635ff7a6c8657c7337a42c
Ricky Chittaphone <rickychittaphone@gmail.com>
2021-06-26 14:42:24 -0400

    guildservice and repo

---
+++ maple-syrup-api/Services/IService/IGuildConfigService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace maple_syrup_api.Services.IService
+{
+    public interface IGuildConfigService
+    {
+    }
+}
---
+++ maple-syrup-api/Services/Service/GuildConfigService.cs
@@ -0,0 +1,13 @@
+﻿using maple_syrup_api.Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace maple_syrup_api.Services.Service
+{
+    public class GuildConfigService : IGuildConfigService
+    {
+
+    }
+}
